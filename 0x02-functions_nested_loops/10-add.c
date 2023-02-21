96bd8160c0a8dbda4991cb4fb5ff4922052fb540
aliceada96 <aliceada96@gmail.com>
2023-02-21 21:56:59 +0000

    addition and signing

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @i: interger 1
+ * @j: integer 2
+ *
+ * Description: give 2 integers returns a sum
+ *
+ * Return: sum
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+
+	return (sum);
+}
