438adf67d99a11c157da11e5e3a59bfe463bb5bf
aliceada96 <aliceada96@gmail.com>
2023-02-23 20:45:09 +0000

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,27 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; n < 101; n++)
+	for (n = 1; n <= 100; n++)
 	{
-		if (n % 5 == 0 && n % 3 == 0)
+		if (n % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf("%d", n);
+		}
+		if (n < 100)
+		{
+			putchar(' ');
 		}
 	}
 		printf("\n");
