1e64f1f45adb23e5aeb26fa6c7a161ef48ed88f3
aliceada96 <aliceada96@gmail.com>
2023-02-23 21:48:39 +0000

    __

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,7 +11,7 @@ void print_line(int n)
 	if (n > 0)
 	{
 		int i = 1;
-		
+
 		while (i <= n)
 		{
 			_putchar('_');
@@ -20,5 +20,4 @@ void print_line(int n)
 
 	}
 	_putchar('\n');
-	return (0);
 }
