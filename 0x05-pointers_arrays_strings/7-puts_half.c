3b1a90ffb0cc8c8b8ec723bf092c696aa515747d
aliceada96 <aliceada96@gmail.com>
2023-02-27 22:20:06 +0000

    print half of a atring

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,27 +1,34 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * puts_half - prints the second half of a string followed by a newline
  * @str: string
- * Return: void
  */
+
+
 void puts_half(char *str)
 {
 	int a, b, end;
 
 	a = 0;
+	b = 0;
 
-	while (str[a] != '\0')
+	while (str[a++])
 	{
-		a++;
-	}
-	end = a;
-	b = end / 2;
-
-	while (b <= end)
-	{
-		_putchar(str[b]);
 		b++;
+	}
+
+	if ((b % 2) == 0)
+	{
+		end = b / 2;
+	}
+
+	else
+	{
+		end = (b + 1) / 2;
+
+		for (a = n; a < b; a++)
+			_putchar(str[a]);
 	}
 	_putchar('\n');
 }
