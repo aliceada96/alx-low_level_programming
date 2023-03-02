833bce6f1c99233f96fd9f4db71798a3bbe588a2
aliceada96 <aliceada96@gmail.com>
2023-03-02 20:59:46 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,22 @@
  *
  * Return: 0 if the strings are equal,
  * -ve difference of the 1st unmatched characters if str1<str2
- * +ve difference of the first unmatched characters if str1>str2
+ * i+ve difference of the first unmatched characters if str1>str2
  */
 int _strcmp(char *s1, char *s2)
 {
-	return (*s1 - *s2);
+	int i = 0;
+	int diff;
+
+	while (s1[i] && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			diff = s1[i] - s2[i];
+		i++;
+		return (diff);
 }
