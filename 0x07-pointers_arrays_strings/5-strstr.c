67855b23823c8d5dc495bfef90b74b35d21d0787
aliceada96 <aliceada96@gmail.com>
2023-03-06 11:54:07 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,7 +21,7 @@ char *_strstr(char *haystack, char *needle)
 			do {
 				if (needle[i + 1] == '\0')
 					return (haystack);
-				index++;
+				i++;
 			} while (haystack[i] == needle[i])
 		}
 		haystack++;
