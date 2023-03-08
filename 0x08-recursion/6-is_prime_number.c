cb6840aa2d1af4697a08ac840772fb4d5032c61b
aliceada96 <aliceada96@gmail.com>
2023-03-08 21:57:20 +0000

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * is_prime - checks for prime number
+ * @n: number to be checked
+ * @c: number to iterate through
+ *
+ * Return: 1 if prime else 0
+ */
+
 int is_prime(unsigned int n, unsigned int c)
 {
 	if (n % c == 0)
@@ -11,6 +19,13 @@ int is_prime(unsigned int n, unsigned int c)
 	}
 	return(is_prime(n, c + 1));
 }
+
+/**
+ * is_prime_number - checks if a number iss prime
+ * @n: number to be checked
+ *
+ * @Return: 1 if n is a prime number else 0
+ */
 
 int is_prime_number(int n)
 {
