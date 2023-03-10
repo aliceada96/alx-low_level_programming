98b634fc6eb20aa90ca9b66e5542fdd7e01060d6
aliceada96 <aliceada96@gmail.com>
2023-03-10 15:38:01 +0000

    Args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,10 +5,11 @@
  * main - prints the number of arguments passed to it
  * @argc: number of arguments passed to cmd line
  * @argv: array of pointers to strings entered on cmd line
- * Return: 0
+ * Return: always 0
  */
-int main(int argc, char __attribute__((__unused__))*argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
+
 	return (0);
 }
