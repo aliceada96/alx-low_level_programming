506e99b96612d7255472769cb8ed9bf0137c0a99
aliceada96 <aliceada96@gmail.com>
2023-03-10 13:33:38 +0000

    mynenw name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line
+ * @argc: Number of arguments entered on the cmd lne
+ * @argv: array of pointers to the arguments entered on the cmd line
+ *
+ * Return: always 0
+ */
+
+int main(int __attribute__((__unused__)) argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
