028946a6c0f71950087f1b7a838eff51a70d1c55
aliceada96 <aliceada96@gmail.com>
2023-03-16 12:39:55 +0000

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; s1[i]; i++)
 		str[len++] = s1[i];
-	for (i = 0;s2[i]; i++)
+
+	for (i = 0;s2[i] && i < n; i++)
 		str[len++] = s2[i];
 
 	str[len] = '\0';
