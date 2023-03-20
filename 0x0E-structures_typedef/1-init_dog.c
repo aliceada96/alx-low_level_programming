a6382bd8de9191dae10535a4fc4a793d2ddf15d6
Alice Ada <aliceada96@gmail.com>
2023-03-20 10:14:57 -0700

    initialize dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes struct dog
+ * @d: dog to be initialized
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ * Reurn: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != null)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
