f4a6a1ef63392d32697973ad78e28334973bf850
Alice Ada <aliceada96@gmail.com>
2023-03-20 13:54:06 -0700

    free doggo

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,10 +12,17 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d == NULLL)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		d = malloc(sizeof(struct dog));
+
+		if (d == NULL)
+		{
+			return;
+		}
+
 	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
