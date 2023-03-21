74592466b16feb8173c4177119a9b5ef66a0ce3d
aliceada96 <aliceada96@gmail.com>
2023-03-21 09:25:23 +0000

    print dog2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->age < 0)
-		printf(Age: "(nil)\n";
+		printf("Age: (nil)\n");
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 
