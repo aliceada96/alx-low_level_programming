72db73f561598ab864a0ef0f8c251755a231d75f
aliceada96 <aliceada96@gmail.com>
2023-03-22 16:14:17 +0000

    compaare array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an int in an array of ints
+ * @array: array of ints
+ * @size: size of array
+ * @cmp: a pointer to the function used to compare values
+ *
+ * Return: -1 if size <=0, else index of 1st element for which cmp
+ * dsnt return 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NUL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-);
+}
