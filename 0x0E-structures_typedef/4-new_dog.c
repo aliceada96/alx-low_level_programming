3a78daa2e0583171c8181274187c447cb05f1ebb
aliceada96 <aliceada96@gmail.com>
2023-03-22 11:15:15 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,7 +19,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (new_dog == NULL)
 		return (NULL);
-	if (name == NULL || owner == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
 	{
 		free(new_dog);
 		return (NULL);
@@ -51,5 +51,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->age = age;
 	return (new_dog);
 }
-
-
