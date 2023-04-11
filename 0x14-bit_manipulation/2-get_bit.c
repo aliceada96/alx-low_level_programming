8e9bd842b1a0551535a548880294ae7778f1bb0a
Alice Ada <aliceada96@gmail.com>
2023-04-11 00:31:58 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,16 +3,14 @@
 /**
  * get_bit - returns the value of a bit at a given index
  * @index: index of the bit you want to get from 0
- *
+ *@n: bit valuei
  * Return: value of the bit at specified index, else -1 if it fails
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	if ((n & (1 << index)) == 0)
-		return (0);
+	return (n & (1 << index));
 
-	return (0);
 }
