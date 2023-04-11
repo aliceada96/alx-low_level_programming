f48c6dbcb1c41be187861353b8afbc1c480330c1
Alice Ada <aliceada96@gmail.com>
2023-04-11 13:10:10 -0700

    append textt to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends texts at the end of a file
+ * @filename: name of file
+ * @text_content: Null terminated string to add at eof
+ *
+ * Return: 1 on success, -1 on fail
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int makeFile, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	makeFile = open(filename, O_WRONLY | 0_APPEND);
+
+	if (text_content != NULL)
+		len = write(makeFile, text_content, text_content[i]);
+	close(makeFile);
+
+	if (makeFile == -1 || i == -1)
+		return (-1);
+
+	return (1);
+}
