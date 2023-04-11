e74332516d50c19554a0f56ebb8288aa677890ff
Alice Ada <aliceada96@gmail.com>
2023-04-11 13:50:41 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content != NULL)
+	while (text_content[i])
 	{
 		i++;
 		op = open(filename, O_WRONLY | O_APPEND);
