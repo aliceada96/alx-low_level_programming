18c240ac3f06ddc38048f911ee4b981f59dbe5e3
Alice Ada <aliceada96@gmail.com>
2023-04-13 13:56:47 -0700

    Copy file contents

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 
 /**
- * main - copies content from one file to another
- * @argc: number of arguments
+ * main - copies file contents into another file
+ * @argc: argument count
  * @argv: pointer to arguments
  *
  * Return: 0
  */
-#define BUFFER_SIZE 1024
 
-int main( int argc, char *argv)
+int main(int argc, char *argv[])
 {
-	int source, destination, rd, wr;
+	int dest, source, r;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_t\no", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	source = open(argv[1], O_RDONNLY);
+
+	source = open(argv[1], O_RDONLY);
 
 	if (source == -1)
 	{
@@ -28,38 +28,23 @@ int main( int argc, char *argv)
 		exit(98);
 	}
 
-	destination = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC 0664);
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	if (destination == -1)
+	r = read(source, buffer, 1024);
+
+	while (r > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
-		exit(99);
-	}
-
-	rd = read(source, buffer, 1024);
-
-	while (rd > 0)
-	{
-		wr = write(destination, buffer, rd);
-		
-		if (wr != rd)
+		if (dest == -1 || write(dest, buffer, r))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-	}
 
-	if (close(source) == -1)
+	}
+	if (close(source) == -1 || close(dest) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source);
 		exit(100);
 	}
-	
-	if (close(destination) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", destination);
-		exit(100);
-	}
-
 	return (0);
 }
