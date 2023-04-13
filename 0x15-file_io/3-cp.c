e060b741398da4be40974a0ecca88c9d5769d74d
Alice Ada <aliceada96@gmail.com>
2023-04-13 14:19:33 -0700

    Copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,6 +40,11 @@ int main(int argc, char *argv[])
 		}
 
 	}
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	if (close(source) == -1 || close(dest) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source);
