654b8ecb1a3578fdf1724868162809e9b8c88da8
aliceada96 <aliceada96@gmail.com>
2023-05-04 12:50:07 -0700

    bin to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,17 +19,13 @@ unsigned int binary_to_uint(const char *b)
 
 	for (count = 0; b[count] != '\0'; count++)
 	{
-		if (b[count] == '0')
+		if (b[count] != '0' && b[count] != '1')
 		{
-			i = i << 1;
-		}
-		else if (b[count] == '1')
-		{
-			i = i << 1 | b[i] - '0';
+			return (0);
 		}
 		else
 		{
-			return (0);
+			i = (i << 1) | (b[i] - '0');
 		}
 	}
 
