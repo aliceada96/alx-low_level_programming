058cd8610e440194c7005ac21057cd065ea9e71c
ALICE ADA AWUOR <aliceada96@gmail.com>
2023-06-15 21:24:23 +0300

    ADD NODE HEAD

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * add_dnodeint - adds a new node at the begining of a list
@@ -9,21 +10,26 @@
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-
-dlistint_t *new;
+dlistint_t *new, *last;
 
 new = malloc(sizeof(dlistint_t));
-
 if (new == NULL)
-return (NULL);
+	return (NULL);
 
 new->n = n;
-new->prev = NULL;
-new->next = *head;
-if (*head != NULL)
-(*head)->prev = new
+new->next = NULL;
 
+if (*head == NULL)
+{
+new->prev = NULL;
 *head = new;
+return (new);
+}
+last = *head;
+while (last->next != NULL)
+last = last->next;
+last->next = new;
+new->prev = last;
 
 return (new);
 }
